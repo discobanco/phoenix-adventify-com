763d48b4c4eafb938dd924c5d8afd0272d8b0279
discobanco <discobanco@gmail.com>
2022-08-25 05:50:59 +0100

    hacky fix

--- js/main.js
+++ js/main.js
@@ -12,17 +12,16 @@ jQuery(document).ready(function($) {
   $('#main-nav .nav').onePageNav({
     currentClass: 'active',
     scrollOffset: 69,
-  });  
+  });
 });
 
 $(document).ready(function(){
-   
+
   //.parallax(xPosition, speedFactor, outerHeight) options:
   //xPosition - Horizontal position of the element
   //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
   //outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
   $('#top').parallax("50%", 0.4);
-  $('#testimonial').parallax("50%", 0.4);
   $('#download').parallax("50%", 0.4);
 })
 
@@ -41,8 +40,4 @@ $(document).ready(function() {
           $('#download-app1').localScroll({
             duration:1200
           });
-           $('#download-app2').localScroll({
-            duration:1000
-          });
         });
-
